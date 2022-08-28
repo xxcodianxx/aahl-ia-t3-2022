1c5cb6f65adee77171809682b31caff78a5e2846
Codian <44289933+xxcodianxx@users.noreply.github.com>
2022-08-29 01:04:36 +0930

    1am clutch??? (nope)

--- py/taylor.py
+++ py/taylor.py
@@ -1,4 +1,5 @@
 import random
+import sys
 import util
 
 import matplotlib.pyplot as plt
@@ -13,7 +14,7 @@ data = util.read_data()
 #     zip(data['timestamp'], data['right_y'])
 # ))
 
-points = list(zip(data['timestamp'], data['head_y']))
+points = list(zip(data['timestamp'], data[sys.argv[1]]))
 
 # split into x and y
 xs = [point[0] for point in points]
@@ -22,15 +23,16 @@ ys = [point[1] for point in points]
 # plot original data
 plt.scatter(xs, ys, linestyle='dashed', c='lightgray')
 
-DEGREE = 2
-THRESHOLD_Y = 0.02 # head=0.02 right=0.1
+DEGREE = int(sys.argv[2])
 
 data_domain = (min(xs), max(xs))
-data_range = data_domain[1] - data_domain[0]
+data_domain_size = data_domain[1] - data_domain[0]
+
+data_range = (min(ys), max(ys))
 
 def fit_for_bound(lower_bound, upper_bound):
-    # plt.axvline(lower_bound, c='lightgray')
-    # plt.axvline(upper_bound, c='lightgray')
+    plt.axvline(lower_bound, c='lightblue', linestyle='dotted')
+    plt.axvline(upper_bound, c='lightblue', linestyle='dotted')
 
     # restrict domain to bounds
     domain = lambda point: lower_bound < point[0] < upper_bound
@@ -43,14 +45,14 @@ def fit_for_bound(lower_bound, upper_bound):
     s_xs = [point[0] for point in subset]
     s_ys = [point[1] for point in subset]
 
-    # fit a quadratic
+    # fit a polynomial
     poly = np.polyfit(s_xs, s_ys, DEGREE)
 
-    equation = str(Polynomial([round(c, 2) for c in poly])).replace("**", "^")
+    equation = str(Polynomial([round(c, 2) for c in poly])).replace("**", "^").replace(' x', 'x').replace('x^1 ', 'x ')
     plt.plot(s_xs, np.polyval(poly, s_xs), label=equation)
 
 def division_partition(number):
-    step_size = data_range / number
+    step_size = data_domain_size / number
 
     for n in range(number):
         # calculate lower and upper bound
@@ -72,18 +74,20 @@ def peaks_partition():
         next = points[i+1]
 
         if ((cur[1]-next[1]) > 0) == ((cur[1]-last[1]) > 0):
+            # if ((cur[1]-next[1]) < 0):
+            #     continue
             peaks.append(cur[0])
 
     # deduplicate
     unique_peaks = []
     last_peak = 0
     for peak in peaks:
-        if abs(last_peak - peak) > 0.1:
+        if abs(last_peak - peak) > 0.1: # 0.25
             unique_peaks.append(peak)
         last_peak = peak
 
     for peak in unique_peaks:
-        plt.axvline(peak, linestyle='dashed', c='lightgray')
+        plt.axvline(peak, linestyle='dashed', c='pink')
 
     sub = []
     for i in range(len(unique_peaks)):
@@ -108,9 +112,19 @@ def peaks_partition():
         fit_for_bound(low, high)
 
 peaks_partition()
-# division_partition(4)
+# division_partition(6)
+# fit_for_bound(0.0, 0.89)
+# fit_for_bound(0.89, 1.525)
+# fit_for_bound(1.6, 1.98)
+# fit_for_bound(1.8, 2.4)
+# fit_for_bound(1.9, 2.3)
 
 plt.legend()
 plt.ylabel('Height (cm)')
 plt.xlabel('Time (s)')
+plt.title(f'Degree {DEGREE} polynomial fit for vertical right foot displacement (crests & troughs)')
+# plt.title('Bad fit for a quadratic')
+# plt.xlim(data_domain)
+# plt.ylim(data_range)
+# plt.xlim([1.5, 2.5])
 plt.show()
